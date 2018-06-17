e3a7067fb9ac2ec3f208a9c527ee7ba235d9a5fd
Igor Chorazewicz <chorig9@gmail.com>
2018-06-17 18:53:43 +0200

    smart contract integration

---
+++ blockchain/article_abi.py
@@ -0,0 +1,95 @@
+abi = """[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "nModifications",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "Id",
+				"type": "bytes32"
+			}
+		],
+		"name": "update",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "commits",
+		"outputs": [
+			{
+				"name": "ID",
+				"type": "bytes32"
+			},
+			{
+				"name": "Author",
+				"type": "address"
+			},
+			{
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getArticleID",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "Id",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "newId",
+				"type": "bytes32"
+			}
+		],
+		"name": "ArticleUpdated",
+		"type": "event"
+	}
+]
+"""
--- client/client.py
+++ client/client.py
@@ -1,8 +1,9 @@
 import logging
-from web3 import Web3
-from web3 import HTTPProvider
+import os
 import ipfsapi
 import time
+import base58
+from blockchain.blockchain_db import *
 
 LOG = logging.getLogger('client')
 
@@ -20,15 +21,28 @@ class DWClient:
             except ipfsapi.exceptions.ConnectionError:
                 LOG.debug('Waiting for ipfsd to get up...')
                 time.sleep(1)
-        # TODO(pkobielak): TestRPCProvider need workaround to work on Win10
-        # TODO(pkobielak): ganache used instead of TestRPCProvider
-        self.web3 = Web3(HTTPProvider('http://localhost:8545'))
+        self.db = BlockchainDB()
+
+    # we remove 2 most significant bytes (they are always 1220)
+    # when reading from smart contract, this 2 bytes must be appended back to ipfs address
+    def _strip_ipfs_address(self, address):
+        return base58.b58decode(address)[2:]
+
+    def _restore_ipfs_address(self, address):
+        return base58.b58encode(b'\x12\x20' + address).decode()
 
     def add_article(self, article_filepath):
         LOG.debug('Adding article sequence started')
 
+        # TODO - we should not add exisitng files, insted, provide editor and create files ourselves
         article_ipfs = self.ipfs_api.add(article_filepath)
         ipfs_address = article_ipfs['Hash']
 
-        LOG.info('Article added: contract_address=%s ipfs_address=%s',
-                 '', ipfs_address)
+        # TODO - title should not be filepath
+        title = os.path.basename(article_filepath)
+
+        try:
+            self.db.add_article_tx(title, self._strip_ipfs_address(ipfs_address))
+            LOG.info('Article added: title=%s, ipfs_address=%s', title, ipfs_address)
+        except Exception as e:
+            print(e)
