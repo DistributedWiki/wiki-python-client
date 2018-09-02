73c6eaf0fca8663817206962ef58d20aeaaf969f
chorig9 <chorig9@gmail.com>
2018-09-02 22:18:30 +0200

    Don't wait for transaction to complete.
    * Added status list of last N transactions.
    * Added info about estimated tx cost.
    * Refactored some code.
    
    resolves #18

--- client/gui/gui.py
+++ client/gui/gui.py
@@ -3,7 +3,11 @@ import os
 import subprocess
 import sys
 
+from PyQt5 import QtGui
 from PyQt5.Qt import QSize
+from PyQt5.QtWidgets import QTableWidget, QMessageBox
+from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtWidgets import QHeaderView
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QListWidget
@@ -15,7 +19,7 @@ import client.gui.gui_conf as gc
 import common.utils as utils
 from client.client import DWClient
 from client.client_conf import DEV_TOP_LEVEL_ADDRESS
-from client.gui.worker import Worker
+from client.gui.background_worker import BackgroundWorker
 from client.login import Login
 
 LOG = logging.getLogger('gui')
@@ -25,41 +29,51 @@ class GUI(QMainWindow):
     """
     GUI for Distributed Wikipedia Client
     """
+
     def __init__(self):
         super().__init__()
         self.resize(gc.FRAME_WIDTH, gc.FRAME_HEIGHT)
         self.move(
-            gc.FHD_W/2 - gc.FRAME_WIDTH/2, gc.FHD_H/2 - gc.FRAME_HEIGHT/2
+            gc.FHD_W / 2 - gc.FRAME_WIDTH / 2,
+            gc.FHD_H / 2 - gc.FRAME_HEIGHT / 2
         )
         self.setWindowTitle(gc.WINDOW_TITLE)
 
-        self.client = None
         self._constructUI()
-        self.worker = None
+
+        self.client = None
         self.article_version_history = None
 
         os.chdir(utils.get_prefix_path())
 
-    def _show_articles_list(self, articles, prefix):
-        for article in articles:
-            if prefix in article:
-                self.articles_list.addItem(article)
-        self.articles_list.show()
+        self.tx_monitor = BackgroundWorker(
+            job=self._get_actions,
+            job_done=self._process_actions,
+            error=self._actions_fetch_failed,
+            run_in_loop=True)
+
+        self.warmup_worker = None
+        self.worker_update = None
+        self.worker_add = None
+        self.worker_search = None
+        self.worker_get = None
+
+        self.loading_box = None
 
     def _constructUI(self):
-        status_title = QLabel("FullNode:", self)
-        status_title.move(50, 510)
-
-        status_variable = QLabel("active", self)
-        status_variable.setStyleSheet("QLabel {color: green;}")
-        status_variable.move(100, 510)
-
         self.top_level = QLabel(
             "Current top level address: " + DEV_TOP_LEVEL_ADDRESS, self
         )
 
         self.top_level.resize(QSize(400, 25))
         self.top_level.move(50, 20)
+
+        self.estimated_tx_price = QLabel("estimated tx price: loading...", self)
+        self.estimated_tx_price.adjustSize()
+        self.estimated_tx_price.move(550, 505)
+        self.estimated_tx_price.setToolTip("Click to refresh value")
+        self.estimated_tx_price.mousePressEvent = \
+            lambda _: self._update_estimation_price()
 
         self.title_edit = QLineEdit(
             self,
@@ -82,10 +96,10 @@ class GUI(QMainWindow):
         btn_search_article.move(310, 50)
         btn_search_article.clicked.connect(self._search_article_action)
 
-        btn_add_article = QPushButton('Add article', self)
-        btn_add_article.resize(QSize(90, 25))
-        btn_add_article.move(410, 50)
-        btn_add_article.clicked.connect(self._add_article_action)
+        self.btn_add_article = QPushButton('Add article', self)
+        self.btn_add_article.resize(QSize(90, 25))
+        self.btn_add_article.move(410, 50)
+        self.btn_add_article.clicked.connect(self._add_article_action)
 
         version_history_label = QLabel("Version history", self)
         version_history_label.move(410, 95)
@@ -114,7 +128,71 @@ class GUI(QMainWindow):
         self.articles_list.show()
         self.articles_list.itemClicked.connect(self._get_article_action)
 
+        action_status_label = QLabel(
+            "Last " + str(gc.NUMBER_OF_DISPLAYED_TRANSACTIONS) +
+            " blockchain actions", self)
+
+        action_status_label.adjustSize()
+        action_status_label.move(50, 505)
+
+        self.action_status = QTableWidget(self)
+        self.action_status.resize(QSize(710, 175))
+        self.action_status.move(50, 530)
+        self.action_status.setColumnCount(2)
+        self.action_status.horizontalHeader().\
+            setSectionResizeMode(QHeaderView.Fixed)
+        self.action_status.setHorizontalHeaderLabels(['Action', 'Status'])
+        self.action_status.setColumnWidth(0, 473)
+        self.action_status.setColumnWidth(1, 220)
+
         self.statusBar().showMessage('Connected')
+
+    def _show_articles_list(self, articles, prefix):
+        for article in articles:
+            if prefix in article:
+                self.articles_list.addItem(article)
+        self.articles_list.show()
+
+    def _set_estimated_price(self, price):
+        text = "estimated tx price: {0:.3f} gwei".format(price / 10**9)
+        self.estimated_tx_price.setText(text)
+        self.estimated_tx_price.adjustSize()
+
+    def _add_action(self, action, status):
+        row = self.action_status.rowCount()
+        self.action_status.insertRow(row)
+
+        self.action_status.setItem(row, 0, QTableWidgetItem(action))
+        self.action_status.setItem(row, 1, QTableWidgetItem(status))
+
+        if status == 'failed':
+            self.action_status.item(row, 1).\
+                setBackground(QtGui.QColor(200, 0, 0))
+        elif status == 'success':
+            self.action_status.item(row, 1).\
+                setBackground(QtGui.QColor(0, 200, 0))
+
+    def _get_actions(self):
+        return self.client.get_unprocessed_tx(
+            gc.NUMBER_OF_DISPLAYED_TRANSACTIONS)
+
+    def _actions_fetch_failed(self, cause):
+        self.action_status.clear()
+        self.action_status.setHorizontalHeaderLabels(['Action', 'Status'])
+        self._add_action(
+            "Failed to fetch transactions, check your connection", cause)
+        self.action_status.setRowCount(gc.NUMBER_OF_DISPLAYED_TRANSACTIONS)
+
+    def _process_actions(self, action_list):
+        self.action_status.clear()
+        self.action_status.setHorizontalHeaderLabels(['Action', 'Status'])
+        for action in action_list:
+            self._add_action(action['description'], action['status'])
+
+        self.action_status.sortItems(0)
+        self.action_status.sortItems(1)
+
+        self.action_status.setRowCount(gc.NUMBER_OF_DISPLAYED_TRANSACTIONS)
 
     def _set_app_status(self, text):
         self.statusBar().showMessage(
@@ -138,35 +216,32 @@ class GUI(QMainWindow):
         )
 
     def _client_action_failed(self, cause):
-        self._set_current_article_title("<none>")
+        self._close_loading()
         self._set_app_status('Error occurred: {}'.format(cause))
-
-    def _update_article_action_success(self, title):
-        self.client.initialize_article_data(title)
-        self.version_history_list.clear()
-        self.version_history_list.addItems(
-            self.client.get_versions_list()
-        )
-        self._set_app_status('Article updated successfully')
 
     def _update_article_action(self):
         LOG.info('_update_article_action called')
         self._set_app_status('Updating article...')
+
         title = self.title_edit.text()
+
         if title is None:
             LOG.error('Article title is not set. Please load article first.')
             return
         path = self._open_file(title)
+
         self._set_app_status(
             'Synchronizing article with distributed systems...'
         )
 
-        self.worker = Worker(self.client.update_article, title, path)
-        self.worker.signal_finished.connect(
-            lambda: self._update_article_action_success(title)
+        self.worker_update = BackgroundWorker(
+            job=lambda: self.client.update_article(title, path),
+            job_done=lambda result: self._close_loading(),
+            error=self._client_action_failed
         )
-        self.worker.signal_error.connect(self._client_action_failed)
-        self.worker.start()
+        self.worker_update.start()
+
+        self._show_loading("Updating article on IPFS")
 
     def _open_file(self, filename):
         """
@@ -186,13 +261,19 @@ class GUI(QMainWindow):
         self._set_app_status('File closed')
         return path
 
-    def _add_article_action_success(self, title):
-        self.client.initialize_article_data(title)
-        self.version_history_list.clear()
-        self.version_history_list.addItems(
-            self.client.get_versions_list()
-        )
-        self._set_app_status('Article added successfully')
+    def _show_loading(self, message):
+        self.loading_box = QMessageBox()
+        self.loading_box.setIcon(QMessageBox.Information)
+        self.loading_box.setStandardButtons(QMessageBox.Close)
+        self.loading_box.button(QMessageBox.Close).setDisabled(True)
+        self.loading_box.setText(message)
+        self.loading_box.setWindowTitle("Warning")
+        self.loading_box.exec()
+
+    def _close_loading(self):
+        if self.loading_box is not None:
+            self.loading_box.close()
+        self.loading_box = None
 
     def _add_article_action(self):
         LOG.debug('_add_article_action called')
@@ -203,16 +284,17 @@ class GUI(QMainWindow):
             'Synchronizing article with distributed systems...'
         )
 
-        self.worker = Worker(self.client.add_article, title, path)
-        self.worker.signal_finished.connect(
-            lambda: self._add_article_action_success(title)
+        self.worker_add = BackgroundWorker(
+            job=lambda: self.client.add_article(title, path),
+            job_done=lambda result: self._close_loading(),
+            error=self._client_action_failed
         )
-        self.worker.signal_error.connect(self._client_action_failed)
-        self.worker.start()
+        self.worker_add.start()
+
+        self._show_loading("Adding article to IPFS")
 
     def _get_article_action_success(self, title):
         self.client.initialize_article_data(title)
-
         self.btn_update_article.setDisabled(False)
 
         self.version_history_list.addItems(
@@ -231,12 +313,16 @@ class GUI(QMainWindow):
         self.article_content_editor.clear()
         self.version_history_list.clear()
 
-        self.worker = Worker(self.client.get_article, title)
-        self.worker.signal_finished.connect(
-            lambda: self._get_article_action_success(title)
+        self.worker_get = BackgroundWorker(
+            job=lambda: self.client.get_article(title),
+            job_done=lambda result: self._get_article_action_success(title),
+            error=self._client_action_failed
         )
-        self.worker.signal_error.connect(self._client_action_failed)
-        self.worker.start()
+        self.worker_get.start()
+
+    def _search_article_action_success(self, articles, title):
+        self._show_articles_list(articles, title)
+        self._set_app_status('Listed found articles')
 
     def _search_article_action(self):
         LOG.debug('_search_article_action called')
@@ -246,23 +332,33 @@ class GUI(QMainWindow):
 
         title = self.title_edit.text()
 
-        self.worker = Worker(lambda:
-                             self._show_articles_list(
-                                 self.client.get_titles(), title))
-        self.worker.signal_finished.connect(
-            lambda: self._set_app_status('Listed found articles')
-        )
-        self.worker.signal_error.connect(self._client_action_failed)
-        self.worker.start()
+        self.worker_search = BackgroundWorker(
+            job=lambda: self.client.get_titles(),
+            job_done=lambda result: self._search_article_action_success(
+                result, title),
+            error=self._client_action_failed)
+        self.worker_search.start()
 
     def init_client(self, eth_private_key, eth_provider, top_level_address):
         self._set_app_status('Initializing client...')
         self.client = DWClient(eth_private_key, eth_provider, top_level_address)
         self._set_app_status('Client initialized')
+        self.tx_monitor.start()
+
+        self.warmup_worker = BackgroundWorker(
+            job=self.client.get_transaction_price,
+            job_done=lambda result:  self._set_estimated_price(result),
+            error=lambda error: None
+        )
+        self.warmup_worker.start()
 
     def start(self, app):
         login_window = Login(self.init_client)
-        login_window.exec()
+        result = login_window.exec()
+
+        if result == 0:
+            self.close()
+            sys.exit(0)
 
         self.show()
         sys.exit(app.exec_())
